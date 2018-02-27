e5fc459c02314a3b2993efdf9836f96362a89c37
bardasandrei <fakirnyes@yahoo.com>
2018-02-27 03:09:49 +0200

    j

--- xd.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Bardas on 27-Feb-18.
- */
-var str = "MAFIA";
-var str2 = "OREZ CU LAPTE";
-document.write(str.fontsize(7).fontcolor("pink").bold() +str2.fontsize(7).fontcolor("blue").bold());
-alert('BARDAS E CEL MAI MARE PROGRAMAR');

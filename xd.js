b3f99cdf76098a494e25aa08c1d185129332231c
bardasandrei <fakirnyes@yahoo.com>
2018-02-27 03:00:12 +0200

    xd

--- xd.js
+++ xd.js
@@ -1,4 +1,7 @@
 /**
  * Created by Bardas on 27-Feb-18.
  */
-alert('hello world');
+var str = "MAFIA";
+var str2 = "OREZ CU LAPTE";
+document.write(str.fontsize(7).fontcolor("pink").bold() +str2.fontsize(7).fontcolor("blue").bold());
+alert('BARDAS E CEL MAI MARE PROGRAMAR');

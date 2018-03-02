5fb1c40786bf57e75110885737bb865245172e96
bardasandrei <fakirnyes@yahoo.com>
2018-03-03 00:37:13 +0200

    andreea

--- xd.js
+++ xd.js
@@ -1,7 +1,9 @@
 /**
  * Created by Bardas on 27-Feb-18.
  */
-var str = "MAFIA";
-var str2 = "OREZ CU LAPTE";
-document.write(str.fontsize(7).fontcolor("pink").bold() +str2.fontsize(7).fontcolor("blue").bold());
-alert('BARDAS E CEL MAI MARE PROGRAMAR');
+var str = "<3 <3 <3";
+var str2 = " ILY ";
+str = str.big();
+document.write(str.fontsize(7).fontcolor("magenta").bold());
+alert('TE IUBESC !!!');
+

5ccc78784e1911d476add91b079f866185f87df7
bardasandrei <fakirnyes@yahoo.com>
2018-03-03 00:48:25 +0200

    daaaaaa

--- xd.js
+++ xd.js
@@ -4,6 +4,6 @@
 var str = "<3 <3 <3";
 var str2 = " ILY ";
 str = str.big();
-document.write(str.fontsize(7).fontcolor("magenta").bold());
+document.write(str.fontsize(7).fontcolor("dimgray").bold());
 alert('TE IUBESC !!!');
 
